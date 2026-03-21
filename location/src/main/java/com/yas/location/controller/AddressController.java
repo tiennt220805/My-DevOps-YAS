1f40c8e18c8733d4f914b98883ececee37d64c1a
Phat Nguyen <phatnguyen9725@gmail.com>
2026-03-21 08:33:40 +0700

    update jenkinsfile

--- location/src/main/java/com/yas/location/controller/AddressController.java
+++ location/src/main/java/com/yas/location/controller/AddressController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-// test
+
 @RestController
 @RequiredArgsConstructor
 public class AddressController {
