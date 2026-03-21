a5c86bc4d669c8c5decc7ed7dd5e2756a901aa1f
Phat Nguyen <phatnguyen9725@gmail.com>
2026-03-21 08:52:42 +0700

    update jenkinsfile

--- media/src/main/java/com/yas/media/controller/MediaController.java
+++ media/src/main/java/com/yas/media/controller/MediaController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-// test
+
 @Validated
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
