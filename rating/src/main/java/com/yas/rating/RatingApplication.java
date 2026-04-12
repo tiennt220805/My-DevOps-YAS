1af4044f328057beb6c1f43a7f1451e10267c0b9
Tipie <tiennt220805@gmail.com>
2026-04-12 11:39:13 +0700

    trigger rating to test CI

--- rating/src/main/java/com/yas/rating/RatingApplication.java
+++ rating/src/main/java/com/yas/rating/RatingApplication.java
@@ -6,11 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = {"com.yas.rating", "com.yas.commonlibrary"})
-@EnableConfigurationProperties({ServiceUrlConfig.class, CorsConfig.class})
+@SpringBootApplication(scanBasePackages = { "com.yas.rating", "com.yas.commonlibrary" })
+@EnableConfigurationProperties({ ServiceUrlConfig.class, CorsConfig.class })
 public class RatingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RatingApplication.class, args);
     }
+    // Trigger CI
 }
